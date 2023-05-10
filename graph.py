562af29e016b0f8768662b9a55e1d3f91863cb91
Talia Kleinebrg <tkleinberg2025@kehillahstudent.org>
2023-05-10 14:28:27 -0700

    done?

--- graph.py
+++ graph.py
@@ -1,23 +1,3 @@
-# import requests
-# import bs4 
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# session = requests.Session()
-# response = session.get('https://www.currentresults.com/Weather/California/Places/palo-alto-temperatures-by-month-average.php', headers={'User-Agent': 'Mozilla/5.0'})
-
-# print(response.status_code)
-
-# tables = pd.read_html(response.text)
-# print(tables)
-
-# table = tables[0]
-# df = table [["High °F", "Unnamed: 2"]]
-# print(df.info())
-# df.plot(x = "Unnamed: 2", y = "High °F" )
-
-# plt.show()
-
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -33,9 +13,14 @@ table = soup.find('table')
 df = pd.read_html(str(table))[0][['High °F', 'Unnamed: 2']]
 print(df.info())
 
-df.plot(x='Unnamed: 2', y='High °F')
-plt.xticks(range(1, 13), ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+df = df[:-1]
+
+df.plot(x='Unnamed: 2', y='High °F', marker ='.')
+plt.xticks(range(0, 12), ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
 plt.xlabel('Month')
 plt.ylabel('High Temperature (°F)')
 plt.title('Average High Temperatures in Palo Alto, CA')
+
+
 plt.show()
+
